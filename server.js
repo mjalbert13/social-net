93d13fa2cdcfe0dee3b25b9fe52f6f7978d549f3
mjalbert13 <mjalbert13@gmail.com>
2019-09-13 14:55:31 -0400

    started api routes files

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/',(req,res) => res.send('API Runnig'))
 
+//Define routes
+app.use('/api/users', require('./routes/API/users'))
+app.use('/api/posts', require('./routes/API/post'))
+app.use('/api/auth', require('./routes/API/auth'))
+app.use('/api/profile', require('./routes/API/profile'))
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>console.log(`Server started on p0rt: ${PORT}`))
