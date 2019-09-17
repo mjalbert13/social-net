161fddad45d526b10f304052d68d72da235b4387
mjalbert13 <mjalbert13@gmail.com>
2019-09-17 17:00:53 -0400

    cleaned up code

--- routes/API/users.js
+++ routes/API/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
