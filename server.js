ae8f69d1aab04833a9a50126660bd8d516944599
mjalbert13 <mjalbert13@gmail.com>
2019-09-18 12:04:34 -0400

    added react app and changed port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const connectDB =require("./config/db")
+const connectDB =require("./config/db");
+const path = require('path')
 
 const app = express();
 
@@ -17,6 +18,18 @@ app.use('/api/posts', require('./routes/API/post'))
 app.use('/api/auth', require('./routes/API/auth'))
 app.use('/api/profile', require('./routes/API/profile'))
 
-const PORT = process.env.PORT || 3000;
+// if (process.env.NODE_ENV === 'production') {
+//     // Set static folder
+//     app.use(express.static('client/build'));
+  
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+// }
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, ()=>console.log(`Server started on p0rt: ${PORT}`))
