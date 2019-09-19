ecdbb170d8749d10540f0a3177a87f1e2b700546
mjalbert13 <mjalbert13@gmail.com>
2019-09-19 15:40:24 -0400

    added nav auth links and logout button

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
     AUTH_ERROR, 
     USER_LOADED,
     LOGIN_FAIL,
-    LOGIN_SUCCESS
+    LOGIN_SUCCESS, 
+    LOGOUT
 } from './types';
 
 import setAuthToken from '../utils/setAuthToken';
@@ -92,3 +93,8 @@ export const login = ( email, password) => async dispatch =>{
         })
     }
 }
+
+//Logout 
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT})
+} 
