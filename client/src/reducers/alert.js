759b1b722ca13bd51a6dc865f9547ec20aed4ee7
mjalbert13 <mjalbert13@gmail.com>
2019-09-27 17:26:35 -0400

    cleaning

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -2,7 +2,7 @@ import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
 const initialState = [];
 
-export default function(state = initialState, action) {
+export default function(state = [], action) {
   const { type, payload } = action;
 
   switch (type) {
