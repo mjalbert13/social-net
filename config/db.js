284db7560f0b66ebdb4fa34f7d69ebe27014a7c4
mjalbert13 <mjalbert13@gmail.com>
2019-09-30 11:56:14 -0400

    udatae

--- config/db.js
+++ config/db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const config = require('config');
-
-
 const db = config.get('mongoURI');
 
 const connectDB = async() => {
@@ -12,10 +10,11 @@ const connectDB = async() => {
             useUnifiedTopology: true,
             useFindAndModify: false
         })
+        
         console.log('MongoDB Connected')
     } catch(err){
         console.log(err.message)
-        process.emit(1)
+        process.exit(1)
     }
 }
 
